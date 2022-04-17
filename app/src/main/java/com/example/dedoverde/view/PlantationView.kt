a5605b68dc35eb94c293c948f1dc0adfc8c8b92d
maracaja <s.maracaja@aluno.ufabc.edu.br>
2022-04-17 14:45:06 -0300

    view

--- app/src/main/java/com/example/dedoverde/view/PlantationView.kt
+++ app/src/main/java/com/example/dedoverde/view/PlantationView.kt
@@ -6,9 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.dedoverde.databinding.FragmentVisualizarPlantacaoBinding
+import com.example.dedoverde.model.PlotOptimizationResults
+import java.util.Locale
 
 class PlantationView : Fragment() {
     private lateinit var binding: FragmentVisualizarPlantacaoBinding
+
+    // Dados para demonstração
+    private val mock = PlotOptimizationResults(
+        "Plantação 1",
+        50,
+        0.3f,
+        7,
+        8,
+        60,
+        "Rua Casabranca 1000"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +34,12 @@ class PlantationView : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        // TODO: De onde pego os dados?
+        binding.materialToolbar.title = mock.title
+        binding.numeroMaximoCanteirosValue.text = mock.maxPlantAmount.toString()
+        binding.espacoMinimoCanteirosValue.text = String.format(Locale.ENGLISH, "%.2f m", mock.minPlantDistance)
+        binding.numeroMaximoColunasValue.text = mock.maxCols.toString()
+        binding.numeroMaximoLinhasValue.text = mock.maxRows.toString()
+        binding.tempoEstimadoColheitaValue.text = String.format(Locale.ENGLISH, "%d dias", mock.estimatedHarvestTime)
+        binding.localizacaoValue.text = mock.address
     }
 }
