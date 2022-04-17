54ccc058bc9488fdad9c3965d48d06bc6d050036
maracaja <s.maracaja@aluno.ufabc.edu.br>
2022-04-17 14:13:43 -0300

    Adicionando arquivos à branch certa

---
+++ app/src/main/java/com/example/dedoverde/view/PlantationView.kt
@@ -0,0 +1,26 @@
+package br.edu.ufabc.dedoverde.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.edu.ufabc.dedoverde.databinding.FragmentVisualizarPlantacaoBinding
+
+class PlantationView : Fragment() {
+    private lateinit var binding: FragmentVisualizarPlantacaoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentVisualizarPlantacaoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // TODO: De onde pego os dados?
+    }
+}
