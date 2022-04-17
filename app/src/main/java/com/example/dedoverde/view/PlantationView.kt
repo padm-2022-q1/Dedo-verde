5b9b39981a60e082e3b29978a1940f1b5116133b
maracaja <s.maracaja@aluno.ufabc.edu.br>
2022-04-17 14:16:43 -0300

    Retirando bugs

--- app/src/main/java/com/example/dedoverde/view/PlantationView.kt
+++ app/src/main/java/com/example/dedoverde/view/PlantationView.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import br.edu.ufabc.dedoverde.databinding.FragmentVisualizarPlantacaoBinding
+import com.example.dedoverde.databinding.FragmentVisualizarPlantacaoBinding
 
 class PlantationView : Fragment() {
     private lateinit var binding: FragmentVisualizarPlantacaoBinding
